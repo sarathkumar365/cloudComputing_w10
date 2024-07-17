e1d142d0cb2314d6d0b7845747a714395d596688
sarathkumarks365 <sarathkumark.s365@gmail.com>
2024-07-17 11:30:07 -0400

    azure login fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Database configuration
 const config = {
     user: 'azureuser',
-    password: 'P@ssw0rdssss',
+    password: 'Sarath@123',
     server: 'wk10342.database.windows.net',
     database: 'wk10',
   options: {
